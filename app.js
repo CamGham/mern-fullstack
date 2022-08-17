b24bb02a99f3289be12e13dfc92106d8bb71770a
CamGham <greayham@gmail.com>
2022-08-17 12:51:55 +1200

    possbile fix

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable prefer-template */
+/* eslint-disable no-path-concat */
+/* eslint-disable prettier/prettier */
 // app.js
 
 const express = require('express');
 const cors = require('cors');
-const path = require("path");
+// const path = require("path");
 const connectDB = require('./config/db');
 
 
@@ -21,7 +24,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
@@ -31,23 +34,24 @@ const port = process.env.PORT || 5001;
 
 
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./front-end/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./front-end/build", "index.html"));
-});
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./front-end/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./front-end/build", "index.html"));
+// });
 
+
+connectDB();
+if (process.env.NODE_ENV === 'production') {
+  // serve front-end client from build folder
+  app.use(express.static(__dirname+'/../front-end/build'));
+  app.get('*', (req, res) =>{
+    res.sendFile(__dirname+'/../client/front-end/index.html')
+  });
+  
+} else {
+  app.get('/', (req, res) => res.send(`API running on port ${port}`));
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-// connectDB();
-// if(process.env.NODE_ENV == 'production'){
-//   app.use(express.static(__dirname+'/../client/build'));
-//   app.get('*',(req,res) =>{
-//     res.sendFile(__dirname+'/../client/build/index.html')
-//   });
-//   }else{
-//     app.get('/', (req,res) => res,send('API running on port ${port}'))
-//   }
