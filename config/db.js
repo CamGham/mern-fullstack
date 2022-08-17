c7f146b74dab18c26b9cc112faee7f4ce446e69d
CamGham <greayham@gmail.com>
2022-08-17 13:14:50 +1200

    push1

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 const config = require('config');
+
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
